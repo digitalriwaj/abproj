bf70a7bffd5d68490e4aecbe5b204f65e88bf6f4
digitalriwaj <78606053+digitalriwaj@users.noreply.github.com>
2021-12-07 07:35:41 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+   // Modal Image Gallery
+   function onClick(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+    var captionText = document.getElementById("caption");
+    captionText.innerHTML = element.alt;
+  }
+  
+  
+  var mySidebar = document.getElementById("mySidebar");
+ //  openingbutton
+  function w3_open() {
+      if (mySidebar.style.display === 'block') {
+          mySidebar.style.display = 'none';
+      } else {
+          mySidebar.style.display = 'block';
+      }
+  }
+ //  closebutton 
+  function w3_close() {
+      mySidebar.style.display = "none";
+  }
